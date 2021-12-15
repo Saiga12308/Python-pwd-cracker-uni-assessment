b6fde859c28e0e1932d594bf9c5dc298d363ca72
Saiga12308 <saiga12308@outlook.com>
2021-12-15 17:16:37 +0000

    main code for password cracker done
    just need to write word_variants.py

--- pwd_crack.py
+++ pwd_crack.py
@@ -1,5 +1,6 @@
 ﻿import cls
 import os
+import multiprocessing
 
 #get pw hash and salt
 #go into dict and get first line
@@ -17,6 +18,21 @@ import os
 #if it gets to end of dict and finds no match, move on to next pw
 
 
+def crack_password(pwds, dict_file):
+    #function for loops for cracking password. done this way to be able to utilise multiprocessing
+
+    #loop of going through each password
+    for line in pwds:
+
+        #0 = id, 1 = my name and student no, 2 = salt, 3 = hashed pwd
+        lst_pwd = line.split(":")
+
+        for line in dict_file:
+
+            #ignoring comments and words too short to be used in a password
+            word = line.strip()
+            if not word.startswith("#") and len(word) >= 7:
+
 
 #getting relative path to the files and opening them
 cur_path = os.path.dirname(__file__)
@@ -27,24 +43,43 @@ dict_path = os.path.relpath("lst\\ow_tiny_lower.lst", cur_path)
 pwds = open(lst_path, "r")
 dict_file = open(dict_path, "r")
 
-#starting loop of going through each password
-for line in pwds:
-
-    #0 = id, 1 = my name and student no, 2 = salt, 3 = hashed pwd
-    lst_pwd = line.split(":")
-
-    for line in dict_file:
-
-        #ignoring comments and words too short to be used in a password
-        word = line.strip()
-        if not word.startswith("#") and len(word) >= 7:
-
-            word_variants = [word]
-
-
-
-
-
+#putting full file into memory (only 41kb, so it won't be an issue)
+lines = pwds.readlines()
 
 pwds.close()
+
+len_pwd_file = len(lines)
+
+#taking sections of it to pass into function so each process can work on 100 passwords at a time
+#this is dynamic, but still assumes that the no of passwords is a multiple of 5. still better than hard-coding ig
+lines1 = lines[0:(len_pwd_file*(1/5))-1]
+lines2 = lines[len_pwd_file*(1/5):(len_pwd_file*(2/5))-1]
+lines3 = lines[len_pwd_file*(2/5):(len_pwd_file*(3/5))-1]
+lines4 = lines[len_pwd_file*(3/5):(len_pwd_file*(4/5))-1]
+lines5 = lines[len_pwd_file*(4/5):len_pwd_file-1]
+
+#clearing the file from memory
+del lines
+
+#creating the processes
+p1 = multiprocessing.Process(target=crack_password, args=(lines1, dict_file))
+p2 = multiprocessing.Process(target=crack_password, args=(lines2, dict_file))
+p3 = multiprocessing.Process(target=crack_password, args=(lines3, dict_file))
+p4 = multiprocessing.Process(target=crack_password, args=(lines4, dict_file))
+p5 = multiprocessing.Process(target=crack_password, args=(lines5, dict_file))
+
+#starting the processes
+p1.start()
+p2.start()
+p3.start()
+p4.start()
+p5.start()
+
+#waits for all processes to finish before continuing
+p1.join()
+p2.join()
+p3.join()
+p4.join()
+p5.join()
+
 dict_file.close()
