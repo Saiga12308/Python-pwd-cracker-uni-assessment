544fdb2bd8dd8582637cc8411fb095bed6208f19
Saiga12308 <saiga12308@outlook.com>
2022-01-10 19:26:17 +0000

    got all working from a single file

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-﻿from ssh import ssh
+﻿from pwd_crack import pwd_crack
+from ssh import ssh
 
 if __name__ == "__main__":
     while True:
         try:
             option = int(input("Which section would you like to run?\n"+
-                               "1. Password cracker (doesn't work)\n"+
+                               "1. Password cracker\n"+
                                "2. SSH client\n"))
 
-            if option != 2:# and option != 1:
+            if option != 1 and option != 2:
                 print("Please only select a given option.\n")
 
             else:
@@ -16,11 +17,33 @@ if __name__ == "__main__":
         except ValueError:
             print("Please only select a given option.\n")
 
-    #can't find a way to 
 
-    #if option == 1:
-    #    pwd = pwd_crack()
-    #    pwd.start()
+    if option == 1:
+        while True:
+            try:
+                process_count = int(input("Select the process count:\n" +
+                                            "1. 10\n" +
+                                            "2. 20\n" +
+                                            "3. 50\n"))
+                if process_count == 1:
+                    pwd = pwd_crack(10)
+                    break
+
+                elif process_count == 2:
+                    pwd = pwd_crack(20)
+                    break
+
+                elif process_count == 3:
+                    pwd = pwd_crack(50)
+                    break
+
+                else:
+                    print("Please only select a value from the list.\n")
+
+            except ValueError:
+                print("Please only select a value from the list.\n")
+        
+        pwd.start()
 
     #This doesn't need to be an elif as I make sure option is either 1 or 2 earlier, but I do so to guarantee that other numbers don't make it through
     if option == 2:
--- pwd_crack.py
+++ pwd_crack.py
@@ -3,106 +3,102 @@ from multiprocessing import Pool
 from datetime import date, timedelta
 from cls.word_variants import WordVarGen
 
-worker_count = 20
+class pwd_crack:
+    def __init__(self, worker_count):
+        #putting full file into memory to divide between threads (only 41kb, so it shouldn't be an issue)
+        pwds = open("A0197423_AIDAN_HERRON_hashed_pw.lst", "r")
+        self.lines = pwds.readlines()
+        pwds.close()
 
-#putting full file into memory to divide between threads (only 41kb, so it shouldn't be an issue)
-pwds = open("A0197423_AIDAN_HERRON_hashed_pw.lst", "r")
-global lines
-lines = pwds.readlines()
-pwds.close()
+        #getting all dates in a list so it doesn't have to be generated each time a process wants to access them
+        self.date_list = []
 
-#getting all dates in a list so it doesn't have to be generated each time a process wants to access them
-global date_list
-date_list = []
+        start_date = date(2000, 1, 1) 
+        end_date = date(2003, 12, 31)
 
-start_date = date(2000, 1, 1) 
-end_date = date(2003, 12, 31)
+        delta = end_date - start_date   # returns timedelta
 
-delta = end_date - start_date   # returns timedelta
+        for i in range(delta.days + 1):
+            day = start_date + timedelta(days=i)
+            day = str(day).split("-")
 
-for i in range(delta.days + 1):
-    day = start_date + timedelta(days=i)
-    day = str(day).split("-")
+            self.date_list.append(day[0]+day[1]+day[2])
+            self.date_list.append(day[2]+day[1]+day[0])
 
-    date_list.append(day[0]+day[1]+day[2])
-    date_list.append(day[2]+day[1]+day[0])
+        self.worker_count = worker_count
+        
 
+    def cls(self):
+        # used later to clear the terminal so it's easy to see when you cracked a password
+        os.system('cls' if os.name=='nt' else 'clear') 
 
-def cls():
-    # used later to clear the terminal so it's easy to see when you cracked a password
-    os.system('cls' if os.name=='nt' else 'clear') 
+    def crack_password(self, pid):
+        #function for loops for cracking password. done this way to be able to utilise multiprocessing
 
-def crack_password(pid):
-    #function for loops for cracking password. done this way to be able to utilise multiprocessing
+        gen = WordVarGen()
 
-    gen = WordVarGen()
+        #used for slices for threads
+        len_pwd_file = len(self.lines)
 
-    #used for slices for threads
-    len_pwd_file = len(lines)
+        slice_a = int((pid/self.worker_count) * len_pwd_file)
+        slice_b = int(((pid+1) / self.worker_count) * len_pwd_file)
 
-    pwds.close()
+        list_pwds = self.lines[slice_a:slice_b]
 
-    slice_a = int((pid/worker_count) * len_pwd_file)
-    slice_b = int(((pid+1) / worker_count) * len_pwd_file)
+        dict_file = open("ow_tiny_lower.lst", "r")
 
-    list_pwds = lines[slice_a:slice_b]
+        for line in dict_file:
 
-    dict_file = open("ow_tiny_lower.lst", "r")
+            #ends the process if it has no more passwords left to crack
+            if len(list_pwds) == 0:
+                break
 
-    for line in dict_file:
+            #ignoring comments in the dictionary file
+            word = line.strip()
+            word = word.replace("\n", "")
 
-        #ends the process if it has no more passwords left to crack
-        if len(list_pwds) == 0:
-            break
+            variants = gen.word_variant_unhashed(word, self.date_list)
 
-        #ignoring comments in the dictionary file
-        word = line.strip()
-        word = word.replace("\n", "")
+            for password in list_pwds:
+                #0 = id, 1 = my name and student no, 2 = salt, 3 = hashed pwd
+                split_pwd = password.split(":")
 
-        variants = gen.word_variant_unhashed(word, date_list)
+                #still had \n at end from file
+                split_pwd[3] = split_pwd[3].replace("\n", "")
 
-        for password in list_pwds:
-            #0 = id, 1 = my name and student no, 2 = salt, 3 = hashed pwd
-            split_pwd = password.split(":")
+                if not word.startswith("#") and len(word) >= 10:
+                    #selects hashing algorithm based on length of the hash
+                    if len(split_pwd[3]) == 32:
 
-            #still had \n at end from file
-            split_pwd[3] = split_pwd[3].replace("\n", "")
+                        print("md5 "+split_pwd[2]+" "+str(pid)+" "+word+"\n")
+                        hashes = gen.word_variant_hashed(variants, split_pwd[2])
 
-            if not word.startswith("#") and len(word) >= 10:
-                #selects hashing algorithm based on length of the hash
-                if len(split_pwd[3]) == 32:
+                    elif len(split_pwd[3]) == 64:
 
-                    print("md5 "+split_pwd[2]+" "+str(pid)+" "+word+"\n")
-                    hashes = gen.word_variant_hashed(variants, split_pwd[2])
+                        print("sha256 "+split_pwd[2]+" "+str(pid)+" "+word+"\n")
+                        hashes = gen.word_variant_hashed(variants, split_pwd[2], False)
 
-                elif len(split_pwd[3]) == 64:
+                    if split_pwd[3] in hashes:
+                        #checks if the hash is in the dictionary returned, then adds to a file if it does
+                        correct_passwords = open("A0197423_Aidan_Herron_cracked_pw.lst", "a")
 
-                    print("sha256 "+split_pwd[2]+" "+str(pid)+" "+word+"\n")
-                    hashes = gen.word_variant_hashed(variants, split_pwd[2], False)
+                        correct_passwords.write(split_pwd[0] + ":" + hashes[split_pwd[3]] + "\n")
 
-                if split_pwd[3] in hashes:
-                    #checks if the hash is in the dictionary returned, then adds to a file if it does
-                    correct_passwords = open("A0197423_Aidan_Herron_cracked_pw.lst", "a")
+                        self.cls()
+                        print("Cracked a password! \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
-                    correct_passwords.write(split_pwd[0] + ":" + hashes[split_pwd[3]] + "\n")
+                        correct_passwords.close()
 
-                    cls()
-                    print("Cracked a password! \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                        list_pwds.remove(password)
+                        break
 
-                    correct_passwords.close()
+            del variants
 
-                    list_pwds.remove(password)
-                    break
+        print("Done cracking passwords!"+str(pid))
+        dict_file.close()
 
-        del variants
+    #creating the processes
+    def start(self):
+        pool = Pool(self.worker_count)
 
-    print("Done cracking passwords!"+str(pid))
-    dict_file.close()
-
-#creating the processes
-if __name__ == "__main__":
-    pool = Pool(worker_count)
-
-    pool.map(crack_password, range(worker_count))
-
-    del lines
+        pool.map(self.crack_password, range(self.worker_count))
